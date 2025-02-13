5d2aa888554a4d90823657980882bbde15c2370c
Guilherme <spigoloniguilherme@gmail.com>
2025-02-12 22:01:22 -0300

    Add files via upload

---
+++ aula04/js.js
@@ -0,0 +1,36 @@
+//01
+
+// var n1 = parseInt(prompt('Insira um número inteiro positivo: '));
+
+
+// for (let i = 0; i < n1; i++){
+//     alert(`Contagem regressiva ${i+1}`);
+// }
+
+
+//02
+
+// var n1 = parseFloat(prompt('Primeiro número: '));
+// let operacao = prompt('+,-,/,*');
+// var n2 = parseFloat(prompt('Segundo número: '));
+
+// switch(operacao){
+//     case '+':
+//     resultado = n1 + n2;
+//    alert(`Resultado: ${resultado}`);
+//    break;
+//     case '-':
+//     resultado1 = n1 - n2
+//    alert(`Resultado: ${resultado}`);
+//    break;
+//     case '/':
+//     resultado = n1 / n2
+//    alert(`Resultado: ${resultado}`);
+//    break;
+//     case '*':
+//     resultado = n1 * n2
+//    alert(`Resultado: ${resultado}`);
+//    break;
+//    default:
+//     alert('Insira um operação válida!')
+// }
